bc032ed7ecc510ebfd44135a5c501b1da9d5cab2
emily <emily@bacheconsulting.com>
2023-05-02 19:49:43 +0200

    started working on Digit abstraction

--- java/src/main/java/Ocr.java
+++ java/src/main/java/Ocr.java
@@ -3,6 +3,27 @@ import java.util.ArrayList;
 
 public class Ocr {
 
+    static boolean compareTwoCharArrays(char[][] digit1, char[][] digit2) {
+        for (int row = 0; row < 4; ++row) {
+            for (int col = 0; col < 4; ++col) {
+                if (digit1[row][col] != digit2[row][col])
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    static class Numeral {
+        public Numeral(char[][] characters) {
+
+        }
+        static char[][] zero = {
+                " _  ".toCharArray(),
+                "| | ".toCharArray(),
+                "|_| ".toCharArray(),
+                "    ".toCharArray()};
+    }
+
     private static final char[][][] NUMERALS = new char[][][] {
         {" _  ".toCharArray(),
          "| | ".toCharArray(),
@@ -48,20 +69,22 @@ public class Ocr {
     public static List<String> parse(String... lines) {
         final List<String> result = new ArrayList<String>();
         for (int i = 0; i < lines.length; i += 4) {
+            int rowOffset = i;
             final char[] work = "             ".toCharArray();
             for (int pos = 0; pos < 9; ++pos) {
+                int columnOffset = 4 * pos;
                 work[pos] = '?';
                 boolean got1 = false;
                 for (int numeral = 0; numeral <= 9; ++numeral) {
                     boolean ok = true;
-                    for (int row = 0; row < 4; ++row) {
-                        for (int col = 0; col < 4; ++col) {
-                            if (NUMERALS[numeral][row][col] != lines[i+row].charAt(4*pos+col))
-                                ok = false;
-                        }
-                    }
+
+                    char[][] sixteenCharactersWereTryingToRecognize = getTheNextSixteenCharacters(rowOffset, columnOffset, lines);
+                    if (!compareTwoCharArrays(NUMERALS[numeral], sixteenCharactersWereTryingToRecognize))
+                        ok = false;
+
                     if (ok) {
-                        work[pos] = (char)(numeral + (int)'0');
+                        char characterForTheNumeralWeFound = ("" + numeral).charAt(0);
+                        work[pos] = characterForTheNumeralWeFound;
                         got1 = true;
                         break;
                     }
@@ -74,5 +97,18 @@ public class Ocr {
             result.add(new String(work));
         }
         return result;
+    }
+
+    private static char[][] getTheNextSixteenCharacters(int rowOffset, int columnOffset, String[] lines) {
+        char[][] sixteenCharactersWereTryingToRecognize = new char[4][];
+        for (int row = 0; row < 4; ++row) {
+            sixteenCharactersWereTryingToRecognize[row] = new char[4];
+            for (int col = 0; col < 4; ++col) {
+                char c = lines[rowOffset + row].charAt(columnOffset + col);
+                sixteenCharactersWereTryingToRecognize[row][col] = c;
+
+            }
+        }
+        return sixteenCharactersWereTryingToRecognize;
     }
 }
